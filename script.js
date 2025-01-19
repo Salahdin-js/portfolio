19c3a92e0a09e67e0d388c9fa995175343f62600
Salahdin-js <salahdinmahoud@gmail.com>
2025-01-19 21:11:26 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ darkModeToggle.addEventListener('click', () => {
 
   if (document.body.classList.contains('dark-mode')) {
     localStorage.setItem('theme', 'dark');
-    darkModeToggle.textContent = mode;
+    darkModeToggle.textContent = ;
   } else {
     localStorage.setItem('theme', 'light');
-    darkModeToggle.textContent = mode;
+    darkModeToggle.textContent = ;
   }
 });
 
@@ -14,8 +14,8 @@ window.addEventListener('DOMContentLoaded', () => {
   const savedTheme = localStorage.getItem('theme');
   if (savedTheme === 'dark') {
     document.body.classList.add('dark-mode');
-    darkModeToggle.textContent = mode;
+    darkModeToggle.textContent = ;
   } else {
-    darkModeToggle.textContent = mode;
+    darkModeToggle.textContent = ;
   }
 });
